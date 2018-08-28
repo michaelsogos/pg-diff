67a1f5471f3f54c845c5c1ce7bc99c4226f25b49
Michael Sogos <michael.sogos@gurustudioweb.it>
2018-08-28 23:47:20 +0200

    * Added option to enable idempotent scripts

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ const compareRecords = require('./src/compareRecords');
 const { Client } = require('pg');
 const log = console.log;
 
-var configName;
-var scriptName;
-var config;
+global.configName = '';
+global.scriptName = '';
+global.config = null;
 
 __printIntro();
 __readArguments();
@@ -26,16 +26,19 @@ __run();
 
 
 function __printHelp() {
-    log(chalk.magenta("|==================|"));
-    log(chalk.magenta("|== pg-diff help ==|"));
-    log(chalk.magenta("|==================|"));
+    log();
+    log();
+    log(chalk.magenta("=============================="));
+    log(chalk.magenta("===   pg-diff-cli   HELP   ==="));
+    log(chalk.magenta("=============================="));
     log();
     log(chalk.gray('OPTION     \t\tDESCRIPTION'));
-    log(chalk.green('-h, --help \t\t') + chalk.blue('To show this help.'));
+    log(chalk.green('-h, --help\t\t') + chalk.blue('To show this help.'));
     log();
     log();
     log(chalk.gray("USAGE:   ") + chalk.yellow("pg-diff ") + chalk.cyan("{configuration name | or valid option} {script name}"));
     log(chalk.gray("EXAMPLE: ") + chalk.yellow("pg-diff ") + chalk.cyan("development my-script"));
+    log();
     log();
 }
 
@@ -53,56 +56,67 @@ function __printIntro() {
 function __printOptions() {
     log();
     log(chalk.gray('CONFIGURED OPTIONS'))
-    log(chalk.yellow("    Script Author: ") + chalk.green(config.options.author));
-    log(chalk.yellow(" Output Directory: ") + chalk.green(path.resolve(process.cwd(), config.options.outputDirectory)));
-    log(chalk.yellow("Schema Namespaces: ") + chalk.green(config.options.schemaNamespace));
-    log(chalk.yellow("     Data Compare: ") + chalk.green(config.options.dataCompare.enable ? 'ENABLED' : 'DISABLED'));
+    log(chalk.yellow("    Script Author: ") + chalk.green(global.config.options.author));
+    log(chalk.yellow(" Output Directory: ") + chalk.green(path.resolve(process.cwd(), global.config.options.outputDirectory)));
+    log(chalk.yellow("Schema Namespaces: ") + chalk.green(global.config.options.schemaNamespace));
+    log(chalk.yellow("Idempotent Script: ") + chalk.green(global.config.options.idempotent ? 'ENABLED' : 'DISABLED'));
+    log(chalk.yellow("     Data Compare: ") + chalk.green(global.config.options.dataCompare.enable ? 'ENABLED' : 'DISABLED'));
     log();
 }
 
 function __readArguments() {
     var args = process.argv.slice(2);
-    if (args.length != 2) {
+    if (args.length <= 0) {
         log(chalk.red('Missing arguments!'));
         __printHelp();
         process.exit();
     }
 
-    switch (args[0]) {
-        case '-h':
-        case '--help':
-            {
+    if (args.length == 1)
+        switch (args[0]) {
+            case '-h':
+            case '--help':
                 __printHelp();
                 process.exit();
-            }
-        default:
-            configName = args[0];
-            scriptName = args[1];
+        }
+
+    if (args.length == 2) {
+        global.configName = args[0];
+        global.scriptName = args[1];
     }
 }
 
 function __loadConfig() {
     try {
-        config = require(path.resolve(process.cwd(), 'pg-diff-config.json'));
-        if (!config[configName])
-            throw new Error(`Impossible to find the configuration with name ${configName} !`);
+        let configFile = require(path.resolve(process.cwd(), 'pg-diff-config.json'));
+        if (!configFile[global.configName])
+            throw new Error(`Impossible to find the configuration with name ${global.configName} !`);
 
-        config = config[configName];
+        global.config = configFile[global.configName];
 
-        if (!config.options)
+        if (!global.config.options)
             throw new Error('The configuration section "options" must exists !');
 
-        if (!config.options.outputDirectory)
-            throw new Error('The configuration section "options" must contains property "outputDirectory" !');
+        if (!global.config.options.outputDirectory)
+            throw new Error('The configuration section "options" must contains property "outputDirectory (string)" !');
 
-        if (!config.options.schemaNamespace)
-            throw new Error('The configuration section "options" must contains property "schemaNamespace" !');
+        if (!global.config.options.schemaNamespace)
+            throw new Error('The configuration section "options" must contains property "schemaNamespace (array of strings)" !');
 
-        if (!config.source)
-            throw new Error('The configuration doesn\'t contains the section "source" !');
+        if (!global.config.options.hasOwnProperty('idempotent'))
+            throw new Error('The configuration section "options" must contains property "idempotent (boolean)" !');
 
-        if (!config.target)
-            throw new Error('The configuration doesn\'t contains the section "target" !');
+        if (!global.config.options.dataCompare)
+            throw new Error('The configuration section "options" must contains property "dataCompare (object)" !');
+
+        if (!global.config.options.dataCompare.hasOwnProperty('enable'))
+            throw new Error('The configuration section "options.dataCompare" must contains property "enable (boolean)" !');
+
+        if (!global.config.source)
+            throw new Error('The configuration doesn\'t contains the section "source (object)" !');
+
+        if (!global.config.target)
+            throw new Error('The configuration doesn\'t contains the section "target (object)" !');
 
     } catch (e) {
         __handleError(e);
@@ -116,24 +130,24 @@ function __initDbConnections() {
     spinner.start();
 
     global.sourceClient = new Client({
-        user: config.source.user,
-        host: config.source.host,
-        database: config.source.database,
-        password: config.source.password,
-        port: config.source.port,
+        user: global.config.source.user,
+        host: global.config.source.host,
+        database: global.config.source.database,
+        password: global.config.source.password,
+        port: global.config.source.port,
     })
     global.sourceClient.connect();
-    log(chalk.whiteBright(`Connected to [${config.source.host}:${config.source.port}/${config.source.database}] `) + chalk.green('✓'));
+    log(chalk.whiteBright(`Connected to [${global.config.source.host}:${global.config.source.port}/${global.config.source.database}] `) + chalk.green('✓'));
 
     global.targetClient = new Client({
-        user: config.target.user,
-        host: config.target.host,
-        database: config.target.database,
-        password: config.target.password,
-        port: config.target.port,
+        user: global.config.target.user,
+        host: global.config.target.host,
+        database: global.config.target.database,
+        password: global.config.target.password,
+        port: global.config.target.port,
     });
     global.targetClient.connect();
-    log(chalk.whiteBright(`Connected to [${config.target.host}:${config.target.port}/${config.target.database}] `) + chalk.green('✓'));
+    log(chalk.whiteBright(`Connected to [${global.config.target.host}:${global.config.target.port}/${global.config.target.database}] `) + chalk.green('✓'));
 
     spinner.stop();
 }
@@ -142,12 +156,12 @@ async function __run() {
     try {
         log();
         log(chalk.yellow("Collect SOURCE database objects"));
-        let sourceSchema = await schema.collectSchemaObjects(sourceClient, config.options.schemaNamespace);
+        let sourceSchema = await schema.collectSchemaObjects(sourceClient, global.config.options.schemaNamespace);
 
         log();
         log();
         log(chalk.yellow("Collect TARGET database objects"));
-        let targetSchema = await schema.collectSchemaObjects(targetClient, config.options.schemaNamespace);
+        let targetSchema = await schema.collectSchemaObjects(targetClient, global.config.options.schemaNamespace);
 
         log();
         log();
@@ -156,23 +170,23 @@ async function __run() {
 
         //console.dir(scripts, { depth: null });
 
-        if (config.options.dataCompare.enable) {
+        if (global.config.options.dataCompare.enable) {
             global.dataTypes = (await sourceClient.query(`SELECT oid, typcategory FROM pg_type`)).rows;
 
             log();
             log();
             log(chalk.yellow("Collect SOURCE tables records"));
-            let sourceTablesRecords = await data.collectTablesRecords(sourceClient, config.options.dataCompare.tables);
+            let sourceTablesRecords = await data.collectTablesRecords(sourceClient, global.config.options.dataCompare.tables);
 
             log();
             log();
             log(chalk.yellow("Collect TARGET tables records"));
-            let targetTablesRecords = await data.collectTablesRecords(targetClient, config.options.dataCompare.tables);
+            let targetTablesRecords = await data.collectTablesRecords(targetClient, global.config.options.dataCompare.tables);
 
             log();
             log();
             log(chalk.yellow("Compare SOURCE with TARGET database table records"));
-            scripts = scripts.concat(compareRecords.compareTablesRecords(config.options.dataCompare.tables, sourceTablesRecords, targetTablesRecords));
+            scripts = scripts.concat(compareRecords.compareTablesRecords(global.config.options.dataCompare.tables, sourceTablesRecords, targetTablesRecords));
         } else {
             log();
             log();
@@ -209,8 +223,8 @@ function __handleError(e) {
 async function __saveSqlScript(scriptLines) {
     return new Promise((resolve, reject) => {
         const now = new Date()
-        const fileName = `${(now).toISOString().replace(/[-:\.TZ]/g, '')}_${scriptName}.sql`;
-        const scriptPath = path.resolve(process.cwd(), config.options.outputDirectory, fileName);
+        const fileName = `${(now).toISOString().replace(/[-:\.TZ]/g, '')}_${global.scriptName}.sql`;
+        const scriptPath = path.resolve(process.cwd(), global.config.options.outputDirectory, fileName);
 
         var file = fs.createWriteStream(scriptPath);
 
@@ -218,10 +232,10 @@ async function __saveSqlScript(scriptLines) {
 
         file.on('finish', () => resolve(scriptPath));
 
-        let titleLength = config.options.author.length > (now).toISOString().length ? config.options.author.length : (now).toISOString().length;
+        let titleLength = global.config.options.author.length > (now).toISOString().length ? global.config.options.author.length : (now).toISOString().length;
 
         file.write(`/******************${'*'.repeat(titleLength+2)}***/\n`);
-        file.write(`/*** SCRIPT AUTHOR: ${config.options.author.padEnd(titleLength)} ***/\n`);
+        file.write(`/*** SCRIPT AUTHOR: ${global.config.options.author.padEnd(titleLength)} ***/\n`);
         file.write(`/***    CREATED ON: ${(now).toISOString().padEnd(titleLength)} ***/\n`);
         file.write(`/******************${'*'.repeat(titleLength+2)}***/\n`);
 
