908126bcde8c9fa45b64269af8d844d8bd6fdbee
Jonathon Cwik <jonathon@shoppinggives.com>
2020-05-14 13:21:42 -0500

    allow dataCompare to be false

--- src/ConfigHandler.js
+++ src/ConfigHandler.js
@@ -9,13 +9,13 @@ class ConfigHandler {
 	 */
 	static LoadConfig(configName) {
 		let configFile = require(path.resolve(process.cwd(), "pg-diff-config.json"));
-		if (!configFile[configName]) throw new Error(`Impossible to find the configuration with name ${configName} !`);
+		if (!configFile[configName]) throw new Error(`Not able to find the configuration with name ${configName} !`);
 
 		let config = configFile[configName];
 
-		if (!config.sourceClient) throw new Error('The configuration doesn\'t contains the section "sourceClient {object}" !');
+		if (!config.sourceClient) throw new Error('The configuration doesn\'t contain the section "sourceClient {object}" !');
 
-		if (!config.targetClient) throw new Error('The configuration doesn\'t contains the section "targetClient {object}" !');
+		if (!config.targetClient) throw new Error('The configuration doesn\'t contain the section "targetClient {object}" !');
 
 		return config;
 	}
@@ -26,22 +26,22 @@ class ConfigHandler {
 	 * @param {Object} config
 	 */
 	static ValidateCompareConfig(config) {
-		if (!config.compareOptions) throw new Error('The configuration doesn\'t contains the section "compareOptions {object}" !');
+		if (!config.compareOptions) throw new Error('The configuration doesn\'t contain the section "compareOptions {object}" !');
 
 		if (!config.compareOptions.outputDirectory)
-			throw new Error('The configuration section "compareOptions" must contains property "outputDirectory {string}" !');
+			throw new Error('The configuration section "compareOptions" must contain property "outputDirectory {string}" !');
 
 		if (!config.compareOptions.schemaCompare)
-			throw new Error('The configuration section "compareOptions" must contains property "schemaCompare {object}" !');
+			throw new Error('The configuration section "compareOptions" must contain property "schemaCompare {object}" !');
 
 		if (!config.compareOptions.schemaCompare.namespaces)
-			throw new Error('The configuration section "compareOptions.schemaCompare" must contains property "namespaces (array of strings}" !');
+			throw new Error('The configuration section "compareOptions.schemaCompare" must contain property "namespaces (array of strings}" !');
 
 		if (!config.compareOptions.dataCompare)
-			throw new Error('The configuration section "optcompareOptionsions" must contains property "dataCompare (object}" !');
+			throw new Error('The configuration section "optcompareOptionsions" must contain property "dataCompare (object}" !');
 
-		if (!config.compareOptions.dataCompare.enable)
-			throw new Error('The configuration section "compareOptions.dataCompare" must contains property "enable (boolean}" !');
+		if (!config.compareOptions.dataCompare.enable && config.compareOptions.dataCompare.enable != false)
+			throw new Error('The configuration section "compareOptions.dataCompare" must contain property "enable (boolean}" !');
 	}
 
 	/**
@@ -50,16 +50,16 @@ class ConfigHandler {
 	 * @param {Object} config
 	 */
 	static ValidateMigrationConfig(config) {
-		if (!config.migrationOptions) throw new Error('The configuration doesn\'t contains the section "migrationOptions {object}" !');
+		if (!config.migrationOptions) throw new Error('The configuration doesn\'t contain the section "migrationOptions {object}" !');
 
 		if (!config.migrationOptions.historyTableSchema)
-			throw new Error('The configuration section "migrationOptions" must contains property "historyTableSchema {string}" !');
+			throw new Error('The configuration section "migrationOptions" must contain property "historyTableSchema {string}" !');
 
 		if (!config.migrationOptions.historyTableName)
-			throw new Error('The configuration section "migrationOptions" must contains property "historyTableName {string}" !');
+			throw new Error('The configuration section "migrationOptions" must contain property "historyTableName {string}" !');
 
 		if (!config.migrationOptions.patchesDirectory)
-			throw new Error('The configuration section "migrationOptions" must contains property "patchesDirectory {string}" !');
+			throw new Error('The configuration section "migrationOptions" must contain property "patchesDirectory {string}" !');
 	}
 }
 
