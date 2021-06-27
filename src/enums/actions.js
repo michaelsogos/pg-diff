1a023cf5e70c9c654c63569b9f7a2438b6b977b1
Michael Sogos <michael.sogos@gurustudioweb.it>
2021-06-27 20:37:23 +0200

    * Updated API package to 1.2.0 * Error will be printed also on sterr #36 * Improved CLI ARGS parser * Added CLI option "-p" to set output folder * Added CLI option "-f" to supply config file full path

---
+++ src/enums/actions.js
@@ -0,0 +1,9 @@
+const actions = {
+	COMPARE: "COMPARE",
+	MIGRATE_TO_SOURCE: "MIGRATE_TO_SOURCE",
+	MIGRATE_TO_TARGET: "MIGRATE_TO_TARGET",
+	SAVE: "SAVE",
+	HELP: "HELP",
+};
+
+module.exports = actions;
